59f924666a57a757162647be2e16f06b9eaacd42
Pete Keen <pete@petekeen.net>
2022-09-18 22:09:03 -0400

    Allow defaults

--- run_compose.sh
+++ run_compose.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 echo "Running pre-start scripts"
-scripts=$(yq '.hosts[env(HOSTNAME)].pre-start // [] | join(" ")' hosts.yml)
+scripts=$(yq '(.defaults.pre-start // []) *+ (.hosts[env(HOSTNAME)].pre-start // []) | join(" ")' hosts.yml)
 
 for script in $scripts; do
     echo "Executing pre-start $script"
@@ -11,7 +11,7 @@ for script in $scripts; do
 done
 
 echo "Copying configs"
-configs=$(yq '.hosts.[env(HOSTNAME)].configs // [] | map("configs/" + .) | join(" ")' hosts.yml)
+configs=$(yq '(.defaults.configs // []) *+ (.hosts.[env(HOSTNAME)].configs // []) | map("configs/" + .) | join(" ")' hosts.yml)
 
 for config in $configs; do
     cp -a /app/$config /configs
@@ -19,19 +19,19 @@ done
 
 echo "Creating .env file"
 echo "__DOCKERSTACK_ENV=1" > /app/.env
-envs=$(yq '.hosts.[env(HOSTNAME)].environment // [] | join("\n")' hosts.yml)
+envs=$(yq '(.defaults.environment // []) *+ (.hosts.[env(HOSTNAME)].environment // []) | join("\n")' hosts.yml)
 printf "$envs" >> /app/.env
 
 echo "Calculating CONFIGS_SHA"
 export CONFIGS_SHA=$(tar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2022-01-01' -C / -cf - /configs /app/.env | sha256sum | cut -d' ' -f1)
 
 echo "Determining stacks"
-stacks=$(yq '.hosts.[env(HOSTNAME)].stacks // [] | map("-f stacks/" + . + ".yml") | join(" ")' hosts.yml)
+stacks=$(yq '(.defaults.stacks // []) *+ (.hosts.[env(HOSTNAME)].stacks // []) | map("-f stacks/" + . + ".yml") | join(" ")' hosts.yml)
 
 base_docker_compose="/usr/bin/docker-compose --ansi never -f docker-compose.yml $stacks"
 
 echo "Setting up crons"
-crons=$(yq ".hosts[env(HOSTNAME)].crons // [] | map(.schedule + \" cd /app && $base_docker_compose run --rm -T \" + .service + \" > /proc/1/fd/1 2>&1\") | join(\"\n\")" hosts.yml)
+crons=$(yq "(.defaults.crons // []) *+ (.hosts[env(HOSTNAME)].crons // []) | map(.schedule + \" cd /app && $base_docker_compose run --rm -T \" + .service + \" > /proc/1/fd/1 2>&1\") | join(\"\n\")" hosts.yml)
 echo "$crons" > /etc/cron.d/dockerstack
 echo "13 2 * * * docker image prune -f" > /etc/cron.d/dockerprune
 
