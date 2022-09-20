51db1e932928fb47571b0509784fec9e17a4c378
Pete Keen <pete@petekeen.net>
2022-09-20 13:15:39 -0400

    Add download_secrets hook

--- start.sh
+++ start.sh
@@ -7,6 +7,10 @@ if test -f /configs/secrets; then
     . /configs/secrets
 fi
 
+if test -f /app/scripts/download_secrets.sh; then
+    . /app/scripts/download_secrets.sh
+fi
+
 /app/run_compose.sh "$@"
 
 if [ -z "$1" ]; then
